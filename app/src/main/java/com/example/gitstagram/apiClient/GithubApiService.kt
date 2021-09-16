7d2672f101cceb662e9cd2c1fee421f8317adbe0
Kevin888369 <kvinbee369@gmail.com>
2021-09-16 16:40:58 +0700

    commit detail done

--- app/src/main/java/com/example/gitstagram/apiClient/GithubApiService.kt
+++ app/src/main/java/com/example/gitstagram/apiClient/GithubApiService.kt
@@ -6,10 +6,10 @@ import retrofit2.http.*
 
 interface GithubApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     suspend fun getSearchUsers(@Query("q") username: String): GitResponse
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     suspend fun getUserDetail(@Path("username") username: String): GitUserDetail
 }
